bd0857cfc8d0763c99fd5cd8d40f98da3bbae6b6
Sky <1989mutian@163.com>
2020-05-18 00:38:21 +0800

    Init version with spring boot

---
+++ src/main/groovy/com/cachetian/code/myobject/MyobjectApplication.groovy
@@ -0,0 +1,13 @@
+package com.cachetian.code.myobject
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MyobjectApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MyobjectApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/cachetian/code/myobject/MyobjectApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.cachetian.code.myobject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyobjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
